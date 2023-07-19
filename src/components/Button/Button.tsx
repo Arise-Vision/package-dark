7a1549cbc3bc90f2309b6463c8ad9ab90cf8ed7d
ArnaudBand <bandonkeyea@gmail.com>
2023-07-19 12:49:15 +0300

    Fix all bugs

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export interface ButtonProps {
   label: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label }) => {
-  return <button>{label}</button>;
-}
+const Button = (props: ButtonProps) => {
+  return <button>{props.label}</button>;
+};
 
 export default Button;
